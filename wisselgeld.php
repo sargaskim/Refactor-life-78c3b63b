75042f27becebdc5b49d19d9813c11462a13135b
sargaskim <2103702@talnet.nl>
2020-03-02 10:57:23 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -19,15 +19,11 @@ calculate($monnies, $input);
 
 function calculate($monnies, $input) {
 
-    foreach($monnies as $coin) {
-        // echo($input . PHP_EOL);
-        if (floor($input / $coin) > 0){
+    foreach ($monnies as $coin) {
+        if (floor($input / $coin) > 0) {
             $times = floor($input / $coin);
             echo "$times x €$coin".PHP_EOL;
             $input -= $times * $coin;
         }
     }
-    // echo($input);
-
-
 }
